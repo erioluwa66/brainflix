b915a980c746e59a6d4ef9fa1ce89b835ad64f09
erioluwa66 <erioluwa66@gmail.com>
2024-02-23 14:26:24 -0500

    comment form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import VideoList from './components/VideoList/VideoList';
+import CommentForm from './components/CommentForm/CommentForm';
 
 function App() {
   return (
    <>
    <Header/>
    <VideoList/>
+   <CommentForm/>
    </>
     
   );
---
+++ src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,29 @@
+import profilepic from "../../assets/images/Mohan-muruge.jpg"
+
+import './CommentForm.scss'
+
+//CommentForm
+function CommentForm({handleSubmit}) {
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        handleSubmit();
+    };
+
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <div>
+                <img src={profilepic} alt ="Mohan muruge"/>
+            </div>
+            <div>
+                <label htmlFor="comment">JOIN THE CONVERSATION</label>
+                <textarea rows="3" name="comment" placeholder="Add a new comment" />
+                <button >COMMENT</button>
+            </div>
+
+        </form>
+    )
+}
+
+export default CommentForm
+
