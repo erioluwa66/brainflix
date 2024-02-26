d01ceb2313bbe4b7358229f6859d5a891e3dd3d1
erioluwa66 <erioluwa66@gmail.com>
2024-02-25 21:52:13 -0500

    next videos adjsuted

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -27,17 +27,3 @@ function VideoList( {videos, selected, handleClick} ) {
 
 export default VideoList;
 
-// function VideoList({}) {
-//     return(
-//         <aside className='video-list'>
-//         <h2 className='video-list__tittle'>NEXT VIDEOS</h2>
-//           {videos.map((video) => (
-//         <div key={video.id}>
-//             <img src={video.image} alt={video.title}/>
-//             {video.title}
-//             {video.channel}
-//         </div>
-//       ))}  
-//     </aside>
-//     )
-// }
