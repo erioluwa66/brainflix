ed8384fc9c6b2ab2d6b5b7fda9db034c83557337
erioluwa66 <erioluwa66@gmail.com>
2024-02-28 15:47:03 -0500

    router added to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Dataset from './data/video-details.json';
 import './App.scss';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from './components/Header/Header';
 import Main from './components/Main/Main'
 
@@ -8,10 +9,13 @@ import Main from './components/Main/Main'
 function App() {
   const [videoData, setVideoData] = useState(Dataset);
   return (
-   <>
-   <Header update={setVideoData}/>
-   <Main videoData={videoData}/>
-   </>
+   <BrowserRouter>
+   {/* <Nav />
+   <Header update={setVideoData}/> */}
+   <Routes>
+   {/* <Main videoData={videoData}/> */}
+   </Routes>
+   </BrowserRouter>
     
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,32 +4,53 @@ import profileImage from '../../assets/images/Mohan-muruge.jpg'
 
 import './NavBar.scss'
 
-
-//navbar element
-const NavBar = () => {
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-    }
-
+function NavBar({ handleSubmit }) {
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+      
+    };
     return (
-        <form className="navbar" onSubmit={handleSubmit}>
+        <form className="navbar" onSubmit={handleFormSubmit}>
             <div className="navbar__input-container">
                 <img alt="magnifier" className="navbar__search-icon" src={searchIcon} />
                 <label className="navbar__label" htmlFor="search"></label>
                 <input className="navbar__input" type="text" name="search" placeholder="Search"/>
             </div>
             
-                <button className="navbar__submit" type="submit">UPLOAD</button>
-                <img className="navbar__submit-icon" src={uploadIcon} alt="upload icon"/>
+            <button className="navbar__submit" type="submit">UPLOAD</button>
+            <img className="navbar__submit-icon" src={uploadIcon} alt="upload icon"/>
                   
-
             <div className="navbar__avatar-box">
                 <img className="navbar__avatar" src={profileImage} alt="Mogan muruge"/>
             </div>
-
         </form>
-    )
+    );
 }
+// //navbar element
+// const NavBar = () => {
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault()
+//     }
+
+//     return (
+//         <form className="navbar" onSubmit={handleSubmit}>
+//             <div className="navbar__input-container">
+//                 <img alt="magnifier" className="navbar__search-icon" src={searchIcon} />
+//                 <label className="navbar__label" htmlFor="search"></label>
+//                 <input className="navbar__input" type="text" name="search" placeholder="Search"/>
+//             </div>
+            
+//                 <button className="navbar__submit" type="submit">UPLOAD</button>
+//                 <img className="navbar__submit-icon" src={uploadIcon} alt="upload icon"/>
+                  
+
+//             <div className="navbar__avatar-box">
+//                 <img className="navbar__avatar" src={profileImage} alt="Mogan muruge"/>
+//             </div>
+
+//         </form>
+//     )
+// }
 
 export default NavBar;
