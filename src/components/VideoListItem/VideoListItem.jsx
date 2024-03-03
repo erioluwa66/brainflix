7a80b5bb51187a5047063de7fa2a7e3828db9907
erioluwa66 <erioluwa66@gmail.com>
2024-03-02 22:37:00 -0500

    image list refactored

--- src/components/VideoListItem/VideoListItem.jsx
+++ src/components/VideoListItem/VideoListItem.jsx
@@ -5,8 +5,8 @@ function VideoListItem( {videoData} ) {
         <article className="video-list__item">
           <img className="video-list__image" src={videoData.image} alt={videoData.title}/>
           <div className="video-list__container">
-            <p className="video-list__title">{videoData.title}</p>
-            <p className="video-list__author">{videoData.channel}</p>
+            <p className="video-list__name">{videoData.title}</p>
+            <p className="video-list__author-name">{videoData.channel}</p>
         </div> 
           
     </article>
